6c4c78d2deec70db9f36c86f168a893157f1c79e
Amal Abdu <abduamal@ymail.com>
2020-05-02 12:38:37 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -49,4 +49,19 @@ class Owner
   def feed_cats
     self.cats.each { |cat| cat.mood = "happy"}
   end
+
+  def sell_pets
+    self.cats.each do |cat|
+      self.cats.delete(cat)
+      cat.owner = nil
+      cat.mood = "nervous"
+    end
+    self.dogs.each do |dog|
+      self.dogs.delete(dog)
+      dog.owner = nil
+      dog.mood = "nervous"
+    end
+  end
+
+  
 end
